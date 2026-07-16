592adc3c8b3dcba9061375b092a4e9ef918aa043
Sean Hardison <seanhardison@gmail.com>
2026-07-16 13:57:13 -0600

    updated MOM6 processing script

--- R/data_processing/MOM6_processing.R
+++ R/data_processing/MOM6_processing.R
@@ -45,45 +45,13 @@ m2023_s <-
   st_as_sf(., coords = c("lon0","lat0"), crs = 4326) %>%
   mutate(year = 2023)
 
-# bind_rows(m2021_s,
-#           m2022_s,
-#           m2023_s) %>%
-#   mutate(t0 = as.Date(t0, format = "%m/%d/%Y")) %>%
-#   group_by(year) %>%
-#   dplyr::summarise(r1 = range(t0)[1],
-#                    r2 = range(t0)[2],
-#                    avg_days = mean(deploy_days))
-#
-# bind_rows(m2021_s,
-#           m2022_s,
-#           m2023_s) %>%
-#   mutate(deploy_t = as.Date(deploy_t, format = "%m/%d/%Y"),
-#          dep_mon = deploy_t) %>%
-#   group_by(year) %>%
-#   dplyr::summarise(r1 = range(t0)[1],
-#                    r2 = range(t0)[2],
-#                    median = median(dep_mon))
-#
-# bind_rows(m2021_s,
-#           m2022_s,
-#           m2023_s) %>%
-#   mutate(t0 = as.Date(t0, format = "%m/%d/%Y"),
-#          deploy_t = as.Date(deploy_t, format = "%m/%d/%Y"),
-#          rel_mon = yday(t0),
-#          dep_mon = yday(deploy_t)) %>%
-#   # group_by(year) %>%
-#   dplyr::summarise(r_dep1 = range(dep_mon)[1],
-#                    r_dep2 = range(dep_mon)[2],
-#                    m_rel = median(rel_mon),
-#                    m_dep = median(dep_mon),
-#                    m = abs(m_rel - m_dep)/7)
 
 ## Extract gridded data to reflect BT during tag release-------`
-fns <- list.files(here::here("~/git/new_mom6/"))
+fns <- list.files("c:/users/seanh/documents/git/new_mom6/")
 bt_all <- NULL
 for (f in seq_along(fns)){
   print(fns[f])
-  bt_init <- stars::read_ncdf(here::here("~/git/new_mom6/",fns[f]),
+  bt_init <- stars::read_ncdf(here::here("c:/users/seanh/documents/git/new_mom6/",fns[f]),
                               var = "tob", proxy = F)
   if (is.null(bt_all)) {
     bt_all <- bt_init
@@ -92,9 +60,10 @@ for (f in seq_along(fns)){
   }
 
 }
+
 # bt_all
 attr(bt_all,"dimensions")[[3]]$values <- seq.Date(from = as.Date("2005-01-01 12:00:00 UTC"),
-                                                  by = "1 day", length.out = 7305)
+                                                  by = "1 day", length.out = 7486)
 # read MOM6 bottom temp data----
 ocean_proj <- read_ncdf(here::here("data/mom6nep_hc202507_ocean_static_ak.nc"),
                         var = c("geolon","geolat"))
@@ -110,9 +79,16 @@ for (i in years){
     message(paste0(i, m))
 
     # extract index for Oct 8-14 in each year
-    bt_ind_which <-
-      which((year(as.POSIXct(attr(bt_all,"dimensions")[[3]]$values)) == i) &
-              (month(as.POSIXct(attr(bt_all,"dimensions")[[3]]$values)) == m))[8:14]
+    if (m == 10){
+      bt_ind_which <-
+        which((year(as.POSIXct(attr(bt_all,"dimensions")[[3]]$values)) == i) &
+                (month(as.POSIXct(attr(bt_all,"dimensions")[[3]]$values)) == m))[8:14]
+    } else if (m == 6){
+      bt_ind_which <-
+        which((year(as.POSIXct(attr(bt_all,"dimensions")[[3]]$values)) == i) &
+                (month(as.POSIXct(attr(bt_all,"dimensions")[[3]]$values)) == m))[1:7]
+    }
+
 
     bt_slice <- bt_all[ , , , bt_ind_which] %>%
       st_as_stars(.,
